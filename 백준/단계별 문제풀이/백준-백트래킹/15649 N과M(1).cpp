af98eeb7bd84409ab3f39c0c8e4de78ed304124f
icefalls <pmhmist@naver.com>
2021-07-21 21:16:44 +0900

    210721 오전 공부 내역입니다.

---
+++ 백준/단계별 문제풀이/백준-백트래킹/15649 N과M(1).cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+#define FAST cin.tie(NULL); ios::sync_with_stdio(false);
+
+int N, M;
+int arr[9] = { 0, };
+bool visited[9] = { 0, };
+
+void dfs(int check)
+{
+	if (check == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			arr[check] = i;
+			dfs(check + 1);
+			visited[i] = false;
+		}
+	}
+
+
+}
+
+int main()
+{
+	FAST;
+	cin >> N >> M;
+	dfs(0);
+	return 0;
+}
