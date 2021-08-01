df82f7362efdcce74ecb11edbce93a995c0a5e2e
icefalls <pmhmist@naver.com>
2021-08-01 19:25:36 +0900

    210801 N과M 응용버전2 공부한 내용입니다.

---
+++ 백준/단계별 문제풀이/백준-백트래킹/15666 N과M(12).cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+#define FAST cin.tie(NULL); ios::sync_with_stdio(false);
+int N, M;
+int arr[9] = { 0, };
+vector <int> v;
+bool visited[9] = { 0, };
+void dfs(int idx, int judge)
+{
+	if (idx == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	int check = -1;
+	for (int i = judge; i < N; i++)
+	{
+		if (v[i] != check)
+		{
+			visited[i] = true;
+			arr[idx] = v[i];
+			check = v[i];
+			dfs(idx + 1,i);
+			visited[i] = false;
+		}
+	}
+
+}
+
+int main()
+{
+	FAST;
+	int temp;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(),v.end());
+	dfs(0,0);
+
+	return 0;
+}
