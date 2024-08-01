df27bd8a9231f80fdc006975854aa7c11303bfaf
pmhmist <pmhmist@naver.com>
2024-08-01 18:20:48 +0900

    1일 1알고리즘

---
+++ 백준/플레/큰 수 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public int solution(){
+
+        int answer = 100;
+
+
+        return answer;
+
+    }
+
+    public static void main(String[] args){
+        Solution s = new Solution();
+    }
+}
